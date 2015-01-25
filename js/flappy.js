bb53c582902597d54d5740a8a8be66afd94ece95
mame0112 <mame0112@gmail.com>
2015-01-25 20:02:38 +0900

    Create index.html basic design

--- js/flappy.js
+++ js/flappy.js
@@ -3,3 +3,15 @@ var app = angular.module('FlappyApplication', []);
 app.controller('FlappyController', function($scope){
 	$scope.message = 'Test message';
 });
+
+app.directive("flappyHeader", function(){
+	return {
+		templateUrl: 'uicomponent/flappyHeader.html'
+	};
+});
+
+app.directive("flappyFooter", function(){
+	return {
+		templateUrl: 'uicomponent/flappyFooter.html'
+	};
+});

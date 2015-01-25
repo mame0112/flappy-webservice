b47362cec0b316ba9a95da78c4b414106568c7c7
mame0112 <mame0112@gmail.com>
2015-01-25 20:25:15 +0900

    Add button click part

--- js/flappy.js
+++ js/flappy.js
@@ -2,14 +2,22 @@ var app = angular.module('FlappyApplication', []);
 
 app.controller('FlappyController', function($scope){
 	$scope.message = 'Test message';
+
+	$scope.buttonClicked = function(){
+		console.log("button clicked");
+		$scope.message = 'Button clicked!';
+	};
+
 });
 
+//Inject Header
 app.directive("flappyHeader", function(){
 	return {
 		templateUrl: 'uicomponent/flappyHeader.html'
 	};
 });
 
+//Inject footer
 app.directive("flappyFooter", function(){
 	return {
 		templateUrl: 'uicomponent/flappyFooter.html'
